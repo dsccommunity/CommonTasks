42ab0d1f5c9762859e04e590989c05a94f29ad61
Raimund Andée [MSFT] <r.andree@live.com>
2018-10-22 21:50:16 +0800

    The DscTagging resource creates a new JEA endpoint now for diagnostics around DSC

--- CommonTasks/DscResources/DscTagging/DscTagging.schema.psm1
+++ CommonTasks/DscResources/DscTagging/DscTagging.schema.psm1
@@ -44,4 +44,21 @@ Configuration DscTagging {
         Ensure    = 'Present'
         Force     = $true
     }
+
+    File DscDiagnosticsRoleCapabilities {
+        SourcePath      = '\\DSCPull01\JEA\DscDiagnostics'
+        DestinationPath = "C:\Program Files\WindowsPowerShell\Modules\DscDiagnostics"
+        Checksum        = 'SHA-1'
+        Ensure          = "Present" 
+        Type            = 'Directory'
+        Recurse         = $true 
+    }
+    
+    JeaEndPoint EndPoint {
+        EndpointName    = "DscDiagnostics"
+        RoleDefinitions = "@{ 'NT AUTHORITY\Authenticated Users' = @{ RoleCapabilities = 'DscDiagnosticsRead' } }"
+        Ensure          = 'Present'
+        #TranscriptDirectory = “$env:ProgramFiles\WindowsPowerShell\Modules\DscDiagnostics\Transcripts”
+        DependsOn       = '[File]DscDiagnosticsRoleCapabilities'
+    }
 }
