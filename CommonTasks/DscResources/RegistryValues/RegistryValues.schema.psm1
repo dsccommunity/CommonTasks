ae9e19a580f48fbaca5c0f21111a98bce3fdddb5
Raimund Andée [MSFT] <raandree@live.com>
2019-05-10 00:11:46 +0200

    Spaces are not allowed in config item names

--- CommonTasks/DscResources/RegistryValues/RegistryValues.schema.psm1
+++ CommonTasks/DscResources/RegistryValues/RegistryValues.schema.psm1
@@ -7,7 +7,7 @@ Configuration RegistryValues {
     Import-DscResource -ModuleName xPSDesiredStateConfiguration -ModuleVersion 8.6.0.0
 
     foreach ($value in $values) {
-        $executionName = $value.Key + '\' + $value.ValueName
+        $executionName = ($value.Key + '\' + $value.ValueName) -replace ' ', ''
         (Get-DscSplattedResource -ResourceName xRegistry -ExecutionName $executionName -Properties $value -NoInvoke).Invoke($value)
     }
 }
