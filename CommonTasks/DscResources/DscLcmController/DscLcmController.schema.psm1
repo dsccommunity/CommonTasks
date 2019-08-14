f59f3318d438b15d71157f8342c782ad09482a01
Raimund Andree <r.andree@live.com>
2019-08-14 10:33:41 +0200

    Fixing typos

--- CommonTasks/DscResources/DscLcmController/DscLcmController.schema.psm1
+++ CommonTasks/DscResources/DscLcmController/DscLcmController.schema.psm1
@@ -40,7 +40,7 @@ $content | Out-File -FilePath $mofFile.FullName -Encoding unicode
 
 Set-DscLocalConfigurationManager -Path $metaMofFolder -Verbose
 
-"$(Get-Date) - Postponed LCM" | Add-Content -Path "$path\LcmPostponeSummery.log"
+"$(Get-Date) - Postponed LCM" | Add-Content -Path "$path\LcmPostponeSummary.log"
 
 Stop-Transcript
 '@
@@ -273,7 +273,7 @@ $logItem = [pscustomobject]@{
     RefreshIntervalOverride          = $refreshIntervalOverride
     RefreshErrors                    = $refreshErrors
     
-} | Export-Csv -Path "$path\LcmControlSummery.txt" -Append
+} | Export-Csv -Path "$path\LcmControllerSummary.txt" -Append
 
 if ($writeTranscripts) {
     Stop-Transcript
