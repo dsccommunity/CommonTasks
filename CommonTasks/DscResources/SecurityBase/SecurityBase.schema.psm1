79ff51db823060cb125e7f765a9ab06a2384fd52
Raimund Andree <r.andree@live.com>
2019-08-20 01:16:58 +0200

    Adding some security settings

--- CommonTasks/DscResources/SecurityBase/SecurityBase.schema.psm1
+++ CommonTasks/DscResources/SecurityBase/SecurityBase.schema.psm1
@@ -6,6 +6,7 @@ Configuration SecurityBase {
     
     Import-DscResource -ModuleName xPSDesiredStateConfiguration
     Import-DscResource -ModuleName ComputerManagementDsc
+    Import-DscResource -ModuleName SecurityPolicyDsc
 
     #Baseline
     xWindowsFeature DisableSmbV1 {
@@ -16,6 +17,18 @@ Configuration SecurityBase {
     PowerShellExecutionPolicy ExecutionPolicyAllSigned {
         ExecutionPolicyScope = 'LocalMachine'
         ExecutionPolicy      = 'RemoteSigned'
+    }
+
+    UserRightsAssignment DenyLogonLocallyForAdministrator
+    {
+        Policy = 'Deny_log_on_locally'
+        Identity = 'contoso\Administrator'
+    }
+
+    UserRightsAssignment AllowLogonLocally
+    {
+        Policy = 'Allow_log_on_locally'
+        Identity = 'Administrators', 'Backup Operators'
     }
     
     #FileServer
@@ -28,6 +41,13 @@ Configuration SecurityBase {
             Ensure    = 'Present'
             Force     = $true
         }
+
+        SecurityOption SecOptions {
+            Name = 'Web Server Secutiry options'
+            Interactive_logon_Message_title_for_users_attempting_to_log_on = 'Secure File Server'
+            Interactive_logon_Message_text_for_users_attempting_to_log_on = 'Your are logging on to a secure file server'
+            Accounts_Rename_administrator_account = 'a'
+        }
     }
 
     #Web Server
@@ -39,6 +59,15 @@ Configuration SecurityBase {
             ValueType = 'Dword'
             Ensure    = 'Present'
             Force     = $true
+        }
+
+        SecurityOption SecOptions {
+            Name = 'Web Server Secutiry options'
+            Interactive_logon_Message_title_for_users_attempting_to_log_on = 'Secure Web Server'
+            Interactive_logon_Message_text_for_users_attempting_to_log_on = 'Your are logging on to a secure web server'
+            Accounts_Rename_administrator_account = 'a'
+            Network_security_LAN_Manager_authentication_level = 'Send NTLMv2 responses only. Refuse LM & NTLM'
+            Network_security_Do_not_store_LAN_Manager_hash_value_on_next_password_change = 'Enabled'
         }
     }
 }
