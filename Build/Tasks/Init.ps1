6d895051fbc63c0df2becdcc4f18c9095c027d30
Raimund Andree <r.andree@live.com>
2019-09-17 02:43:59 +0530

    Considering a path based upon the build.ps1 script to set the build variables

--- Build/Tasks/Init.ps1
+++ Build/Tasks/Init.ps1
@@ -1,16 +1,18 @@
 Task Init {
+
+    Set-Location -Path $ProjectPath
     if (-not $env:BHProjectName) {
         try {
-            Set-BuildEnvironment
-            
+            Write-Host "Calling 'Set-BuildEnvironment' with path '$ProjectPath'"
+            Set-BuildEnvironment -Path $ProjectPath
         }
         catch {
             Write-Host "Error calling 'Set-BuildEnvironment'. The task will probably fail if in build."
+            Write-Host "The error was: $($_.Exception.Message)"
         }
     }
 
     $lines
-    Set-Location -Path $ProjectPath
     "Build System Details:"
     Get-Item -Path env:BH*
     "`n"
