ebe1633b49f3ecef928e9caa568cc616e78a2c08
Raimund Andée [MSFT] <raandree@live.com>
2020-06-22 13:54:27 +0200

    Renamed composite resource 'Domain' to 'AdDomain'

--- CommonTasks/DscResources/AdDomain/AdDomain.schema.psm1
+++ CommonTasks/DscResources/AdDomain/AdDomain.schema.psm1
@@ -1,4 +1,4 @@
-configuration Domain
+configuration AdDomain
 {
     param
     (
---
+++ CommonTasks/Tests/Integration/26 AdDomain.Tests.ps1
@@ -0,0 +1,32 @@
+﻿Import-Module -Name $PSScriptRoot\Assets\TestHelpers.psm1
+Init
+
+Describe "AdDomain DSC Resource compiles" -Tags FunctionalQuality {
+
+    It "AdDomain Compiles" {
+
+        configuration "Config_AdDomain" {
+
+            Import-DscResource -ModuleName CommonTasks
+
+            node "localhost_AdDomain" {
+                AdDomain adDomain {
+                    DomainFqdn = $configurationData.Datum.Config.AdDomain.DomainFqdn
+                    DomainName = $configurationData.Datum.Config.AdDomain.DomainName
+                    DomainDN = $configurationData.Datum.Config.AdDomain.DomainDN
+                    DomainJoinAccount = $configurationData.Datum.Config.AdDomain.DomainJoinAccount
+                    DomainAdministrator = $configurationData.Datum.Config.AdDomain.DomainAdministrator
+                    SafeModePassword = $configurationData.Datum.Config.AdDomain.SafeModePassword
+                    DomainTrust = $configurationData.Datum.Config.AdDomain.DomainTrust
+                }
+            }
+        }
+
+        { & "Config_AdDomain" -ConfigurationData $configurationData -OutputPath $env:BHBuildOutput -ErrorAction Stop } | Should -Not -Throw
+    }
+
+    It "AdDomain should have created a mof file" {
+        $mofFile = Get-Item -Path "$env:BHBuildOutput\localhost_AdDomain.mof" -ErrorAction SilentlyContinue
+        $mofFile | Should -BeOfType System.IO.FileInfo
+    }
+}
