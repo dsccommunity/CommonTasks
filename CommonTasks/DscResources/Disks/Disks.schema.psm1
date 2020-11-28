dd0e038e9821ceca187534b18088de4b24d53e33
Heiko Stehli <heiko.stehli@gmx.de>
2020-11-28 12:48:42 +0100

    - Disks accepts now size value string s with KB/MB/GB - extend Disks test data - added Disks to documentation

--- CommonTasks/DscResources/Disks/Disks.schema.psm1
+++ CommonTasks/DscResources/Disks/Disks.schema.psm1
@@ -10,9 +10,17 @@ configuration Disks
     Import-DscResource -ModuleName StorageDsc
 
     foreach ($disk in $Disks) {
+        # convert string with KB/MB/GB into Uint64
+        if ($null -ne $disk.Size) {
+            $disk.Size = [Uint64] ($disk.Size / 1)
+        }
+        
+        # convert string with KB/MB/GB into Uint32
+        if ($null -ne $disk.AllocationUnitSize) {
+            $disk.AllocationUnitSize = [Uint32] ($disk.AllocationUnitSize / 1)
+        }
 
         $executionName = $disk.DiskId
         (Get-DscSplattedResource -ResourceName Disk -ExecutionName $executionName -Properties $disk -NoInvoke).Invoke($disk)
-
     }
 }
