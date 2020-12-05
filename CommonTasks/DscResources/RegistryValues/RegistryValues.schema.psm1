1c57cc94b27f79c3c0d35d58198d0c577c1f930a
Heiko Stehli <heiko.stehli@gmx.de>
2020-12-05 11:53:42 +0100

    - RegistryValues: add some default values - RegistryValues: add documentation

--- CommonTasks/DscResources/RegistryValues/RegistryValues.schema.psm1
+++ CommonTasks/DscResources/RegistryValues/RegistryValues.schema.psm1
@@ -7,6 +7,16 @@ configuration RegistryValues {
     Import-DscResource -ModuleName xPSDesiredStateConfiguration
 
     foreach ($value in $values) {
+        
+        if (-not $value.ContainsKey('Ensure')) {
+            $value.Ensure = 'Present'
+        }
+        if ([String]::IsNullOrEmpty($value.ValueName)) {
+            $value.ValueName = ''
+        }
+        if ([String]::IsNullOrEmpty($value.ValueData) -and ($value.Ensure -ne 'Present')) {
+            $value.ValueData = ''
+        }
         $executionName = ($value.Key + '\' + $value.ValueName) -replace ' ', ''
         (Get-DscSplattedResource -ResourceName xRegistry -ExecutionName $executionName -Properties $value -NoInvoke).Invoke($value)
     }
