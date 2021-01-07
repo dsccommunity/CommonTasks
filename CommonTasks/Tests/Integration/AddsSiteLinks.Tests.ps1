9264dcd9c4c7305a7e0978048a4ee26a30c69d6f
Raimund Andree <r.andree@live.com>
2021-01-07 14:16:46 +0100

    Added 'AddsSiteLinks' configuration

---
+++ CommonTasks/Tests/Integration/AddsSiteLinks.Tests.ps1
@@ -0,0 +1,26 @@
+﻿Import-Module -Name $PSScriptRoot\Assets\TestHelpers.psm1
+Init
+
+Describe "AddsSiteLinks DSC Resource compiles" -Tags FunctionalQuality {
+
+    It "AddsSiteLinks Compiles" {
+        
+        configuration "Config_AddsSiteLinks" {
+
+            Import-DscResource -ModuleName CommonTasks
+
+            node "localhost_AddsSiteLinks" {
+                AddsSiteLinks AddsSiteLinks {
+                    SiteLinks = $configurationData.Datum.Config.AddsSiteLinks.SiteLinks
+                }
+            }
+        }
+
+        { & "Config_AddsSiteLinks" -ConfigurationData $configurationData -OutputPath $env:BHBuildOutput -ErrorAction Stop } | Should -Not -Throw
+    }
+
+    It "AddsSiteLinks should have created a mof file" {
+        $mofFile = Get-Item -Path "$env:BHBuildOutput\localhost_AddsSiteLinks.mof" -ErrorAction SilentlyContinue
+        $mofFile | Should -BeOfType System.IO.FileInfo
+    }
+}
