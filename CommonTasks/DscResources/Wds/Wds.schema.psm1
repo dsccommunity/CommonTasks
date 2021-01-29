a64f6ed3e2cf1a85faf91e67a48a865980a4b941
Heiko Stehli <heiko.stehli@gmx.de>
2021-01-29 21:14:22 +0100

    add SmbShares & ScheduledTasks, Wds: configuration of DHCP scope can be disabled

--- CommonTasks/DscResources/Wds/Wds.schema.psm1
+++ CommonTasks/DscResources/Wds/Wds.schema.psm1
@@ -2,21 +2,31 @@ configuration Wds
 {
     param
     (
+        [Parameter(Mandatory)]
         [string]
         $RemInstPath,
 
+        [Parameter(Mandatory=$false)]
         [pscredential]
-        $RunAsUser,
+        $RunAsUser = $null,
 
+        [Parameter(Mandatory=$false)]
+        [boolean]
+        $UseExistingDhcpScope = $false,
+
+        [Parameter(Mandatory=$false)]
         [string]
         $ScopeStart,
 
+        [Parameter(Mandatory=$false)]
         [string]
         $ScopeEnd,
 
+        [Parameter(Mandatory=$false)]
         [string]
         $ScopeId,
 
+        [Parameter(Mandatory=$false)]
         [string]
         $SubnetMask
     )
@@ -25,23 +35,52 @@ configuration Wds
     Import-DscResource -ModuleName WdsDsc
     Import-DscResource -ModuleName xDhcpServer
 
-    xDhcpServerScope clientScope
+    if( $UseExistingDhcpScope -eq $false )
     {
-        ScopeId      = $ScopeId
-        IPStartRange = $ScopeStart
-        IPEndRange   = $ScopeEnd
-        SubnetMask   = $SubnetMask
-        Name         = 'WdsClients'
+        WindowsFeature dhcpFeature
+        {
+            Name   = 'DHCP'
+            IncludeAllSubFeature = $true
+            Ensure = 'Present'
+        }
+
+        xDhcpServerScope clientScope
+        {
+            ScopeId      = $ScopeId
+            IPStartRange = $ScopeStart
+            IPEndRange   = $ScopeEnd
+            SubnetMask   = $SubnetMask
+            Name         = 'WdsClients'
+            Ensure       = 'Present'
+            DependsOn    = '[WindowsFeature]dhcpFeature'
+        }
+    }
+    else
+    {
+        if( -not [string]::IsNullOrWhiteSpace($ScopeId) -or
+            -not [string]::IsNullOrWhiteSpace($ScopeStart) -or
+            -not [string]::IsNullOrWhiteSpace($ScopeEnd) -or
+            -not [string]::IsNullOrWhiteSpace($SubnetMask) )
+        {
+            throw "ERROR: if 'UseExistingDhcpScope' is set to 'true' all DHCP scope parameters shall be empty."
+        }
+    }
+
+    WindowsFeature wdsFeature
+    {
+        Name                 = 'WDS'
+        IncludeAllSubFeature = $true
+        Ensure               = 'Present'
     }
 
     WdsInitialize wdsInit
     {
-        DependsOn = '[xDhcpServerScope]clientScope'
         IsSingleInstance     = 'Yes'
         PsDscRunAsCredential = $runAsCred
         Path                 = $RemInstPath
         Authorized           = $true
         Standalone           = $false
         Ensure               = 'Present'
+        DependsOn            = '[WindowsFeature]wdsFeature'
     }
 }
