28b77691d526278cc043e636b75c3def70e06875
Heiko Stehli <heiko.stehli@gmx.de>
2021-02-12 20:56:33 +0100

    fix documentation, WindowsServices & DnsServerRecords

--- CommonTasks/DscResources/DnsServerRecords/DnsServerRecords.schema.psm1
+++ CommonTasks/DscResources/DnsServerRecords/DnsServerRecords.schema.psm1
@@ -9,7 +9,9 @@
 
     Import-DscResource -ModuleName PSDesiredStateConfiguration
     Import-DscResource -ModuleName xDnsServer
-    
+
+    [boolean]$dnsServerInstalled = $false
+
     foreach ($dnsRecord in $Records)
     {
         # Remove Case Sensitivity of ordered Dictionary or Hashtables
@@ -18,6 +20,23 @@
         if (-not $dnsRecord.ContainsKey('Ensure'))
         {
             $dnsRecord.Ensure = 'Present'
+        }
+
+        # install DNS server if DNS record shall be set on a local DNS server
+        if (-not $dnsRecord.ContainsKey('DnsServer') -or $dnsRecord.DnsServer -eq 'localhost')
+        {
+            if( $dnsServerInstalled -eq $false )
+            {
+                WindowsFeature DNSServer
+                {
+                    Name   = 'DNS'
+                    Ensure = 'Present'
+                }
+
+                $dnsServerInstalled = $true
+            }
+
+            $dnsRecord.DependsOn = '[WindowsFeature]DNSServer'
         }
 
         $executionName = "dnsrecord_$("$($dnsRecord.Name)_$($dnsRecord.Zone)" -replace '[()-.:\s]', '_')"
