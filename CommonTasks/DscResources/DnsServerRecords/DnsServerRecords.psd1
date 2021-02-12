ac62a4dc989cb6ec4c2867f6df8e894352488ab8
stehlih <heiko.stehli@gmx.de>
2021-02-12 14:30:18 +0100

    add DnsServerForwarder, DnsServerPrimaryZones and DnsServerRecords

---
+++ CommonTasks/DscResources/DnsServerRecords/DnsServerRecords.psd1
@@ -0,0 +1,9 @@
+﻿@{
+    RootModule           = 'DnsServerRecords.schema.psm1'
+    ModuleVersion        = '0.0.1'
+    GUID                 = '1b76370b-fec9-4c14-bf93-ba03b0a87aa3'
+    Author               = 'NA'
+    CompanyName          = 'NA'
+    Copyright            = 'NA'
+    DscResourcesToExport = @('DnsServerRecords')
+}
---
+++ CommonTasks/DscResources/DnsServerRecords/DnsServerRecords.schema.psm1
@@ -0,0 +1,27 @@
+﻿configuration DnsServerRecords
+{
+    param
+    (
+        [Parameter(Mandatory)]
+        [Hashtable[]]
+        $Records
+    )
+
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+    Import-DscResource -ModuleName xDnsServer
+    
+    foreach ($dnsRecord in $Records)
+    {
+        # Remove Case Sensitivity of ordered Dictionary or Hashtables
+        $dnsRecord = @{}+$dnsRecord
+
+        if (-not $dnsRecord.ContainsKey('Ensure'))
+        {
+            $dnsRecord.Ensure = 'Present'
+        }
+
+        $executionName = "dnsrecord_$("$($dnsRecord.Name)_$($dnsRecord.Zone)" -replace '[()-.:\s]', '_')"
+
+        (Get-DscSplattedResource -ResourceName xDnsRecord -ExecutionName $executionName -Properties $dnsRecord -NoInvoke).Invoke($dnsRecord)
+    }
+}
