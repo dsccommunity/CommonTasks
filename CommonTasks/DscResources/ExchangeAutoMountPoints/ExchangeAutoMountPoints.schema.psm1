09f4dde493ae638634c2ac79b0063ed9dab415c1
Raimund Andrée [MSFT] <r.andree@live.com>
2021-03-24 20:34:13 +0100

    Added new Exchange and PKI configurations (#84)
    * Added 'CertificateAuthorities' configuration
    
    * Added more Exchange configurations
    
    * Fixing ExchangeAutoMountPoints

---
+++ CommonTasks/DscResources/ExchangeAutoMountPoints/ExchangeAutoMountPoints.schema.psm1
@@ -0,0 +1,70 @@
+configuration ExchangeAutoMountPoints {
+    param
+    (
+        [Parameter()]
+        [System.String]
+        $Identity,
+
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $AutoDagDatabasesRootFolderPath,
+
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $AutoDagVolumesRootFolderPath,
+
+        [Parameter(Mandatory = $true)]
+        [System.String[]]
+        $DiskToDBMap,
+
+        [Parameter(Mandatory = $true)]
+        [System.UInt32]
+        $SpareVolumeCount,
+
+        [Parameter()]
+        [System.Boolean]
+        $EnsureExchangeVolumeMountPointIsLast = $false,
+
+        [Parameter()]
+        [System.Boolean]
+        $CreateSubfolders = $false,
+
+        [Parameter()]
+        [ValidateSet('NTFS', 'REFS')]
+        [System.String]
+        $FileSystem = 'NTFS',
+
+        [Parameter()]
+        [System.String]
+        $MinDiskSize = '',
+
+        [Parameter()]
+        [ValidateSet('MBR', 'GPT')]
+        [System.String]
+        $PartitioningScheme = 'GPT',
+
+        [Parameter()]
+        [System.String]
+        $UnitSize = '64K',
+
+        [Parameter()]
+        [System.String]
+        $VolumePrefix = 'EXVOL'
+    )
+
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+    Import-DscResource -Module xExchange
+    
+    if (-not $PSBoundParameters.ContainsKey('Identity')) {
+        $PSBoundParameters.Add('Identity', $Node.NodeName)
+        $executionName = $Node.NodeName
+    }
+    else {
+        $executionName = $Identity
+    }
+    $PSBoundParameters.Remove('InstanceName')
+
+    
+    (Get-DscSplattedResource -ResourceName xExchAutoMountPoint -ExecutionName $executionName -Properties $PSBoundParameters -NoInvoke).Invoke($PSBoundParameters)
+
+}
