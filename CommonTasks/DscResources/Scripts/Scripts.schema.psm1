b956f0411bf9dc761f0698883641fdcc99291fcb
Heiko Stehli <heiko.stehli@gmx.de>
2021-06-06 17:57:21 +0200

    add dynamic parameters to Scripts resource

--- CommonTasks/DscResources/Scripts/Scripts.schema.psm1
+++ CommonTasks/DscResources/Scripts/Scripts.schema.psm1
@@ -7,11 +7,44 @@ configuration Scripts {
     Import-DscResource -ModuleName PSDesiredStateConfiguration
     Import-DscResource -ModuleName xPSDesiredStateConfiguration
 
-    foreach ($item in $Items) {
+    foreach ($item in $Items)
+    {
+        # remove case sensivity from hashtable
+        $item = @{} + $item
+
+        if( $null -ne $item.Params )
+        {
+            $params = '$params = ''' + ($item.Params | ConvertTo-Json -Compress) + "' | ConvertFrom-JSON;`n"
+        }
+
+        if( [string]::IsNullOrWhiteSpace($item.GetScript) )
+        {
+            $item.GetScript = "@{ Result = 'N/A' }"
+        }
+        elseif( $null -ne $params )
+        {
+            $item.GetScript = $params + $item.GetScript
+        }
+
+        if( [string]::IsNullOrWhiteSpace($item.SetScript) )
+        {
+            $item.SetScript = "Write-Verbose 'SetScript is not implemented.'"
+        }
+        elseif( $null -ne $params )
+        {
+            $item.SetScript = $params + $item.SetScript
+        }
+
+        if( $null -ne $params )
+        {
+            $item.TestScript = $params + $item.TestScript
+        }
 
         $executionName = "Script_$($item.Name)"
-        [void]$item.Remove('Name')
-        (Get-DscSplattedResource -ResourceName xScript -ExecutionName $executionName -Properties $item -NoInvoke).Invoke($item)
 
+        [void]$item.Remove('Name')
+        [void]$item.Remove('Params')
+        
+        (Get-DscSplattedResource -ResourceName xScript -ExecutionName $executionName -Properties $item -NoInvoke).Invoke($item)
     }
 }
