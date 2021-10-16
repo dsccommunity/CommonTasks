73d7e136c8218cce9270da25626b698f095045f9
Raimund Andree <r.andree@live.com>
2021-10-16 17:51:18 +0200

    Added 'WaitForAllNodes' resource

---
+++ CommonTasks/DscResources/WaitForAllNodes/WaitForAllNodes.schema.psm1
@@ -0,0 +1,25 @@
+configuration WaitForAllNodes {
+    param (
+        [Parameter()]
+        [hashtable[]]
+        $Items
+    )
+
+    <#
+    NodeName = [string[]]
+    ResourceName = [string]
+    [DependsOn = [string[]]]
+    [PsDscRunAsCredential = [PSCredential]]
+    [RetryCount = [UInt32]]
+    [RetryIntervalSec = [UInt64]]
+    [ThrottleLimit = [UInt32]]
+    #>
+
+    foreach ($item in $items) {
+        $executionName = $item.ResourceName
+        #[void]$item.Remove('InstanceName')
+
+        (Get-DscSplattedResource -ResourceName WaitForAll -ExecutionName $executionName -Properties $item -NoInvoke).Invoke($item)
+    }
+
+}
