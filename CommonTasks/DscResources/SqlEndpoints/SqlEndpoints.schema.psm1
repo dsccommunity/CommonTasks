608121e1668fbe456e9ed3dee761fe5a0eae2fb5
Raimund Andrée [MSFT] <r.andree@live.com>
2021-10-27 08:41:55 +0200

    Added more SQL configurations (#116)
    * Updated SharePointDSC version
    
    * Removing Program Files from PSModulePath to prevent conflicts with duplicate modules
    
    * Merged test files and added an option for skipping module tests
    
    * Added documentation for 'SqlAliases'
    
    * Adding SQL configs and config data
    
    * Added more SQL configurations

---
+++ CommonTasks/DscResources/SqlEndpoints/SqlEndpoints.schema.psm1
@@ -0,0 +1,43 @@
+configuration SqlEndpoints {
+    param (
+        [Parameter()]
+        [String]$DefaultInstanceName = 'MSSQLSERVER',
+
+        [Parameter(Mandatory)]
+        [hashtable[]]$Values
+    )
+
+    <#
+    EndpointName = [string]
+    EndpointType = [string]{ DatabaseMirroring | ServiceBroker }
+    InstanceName = [string]
+    [DependsOn = [string[]]]
+    [Ensure = [string]{ Absent | Present }]
+    [IpAddress = [string]]
+    [IsMessageForwardingEnabled = [bool]]
+    [MessageForwardingSize = [UInt32]]
+    [Owner = [string]]
+    [Port = [UInt16]]
+    [PsDscRunAsCredential = [PSCredential]]
+    [ServerName = [string]]
+    [State = [string]{ Disabled | Started | Stopped }]
+    #>
+    
+    Import-DscResource -ModuleName SqlServerDsc
+
+    foreach ($value in $Values) 
+    {
+        if(-not $value.InstanceName)
+        {
+            $value.InstanceName = $DefaultInstanceName
+        }
+
+        if(-not $value.Ensure)
+        {
+            $value.Ensure = 'Present'
+        }
+
+        $executionName = "$($value.InstanceName)_$($value.EndpointName -replace ' ','')"
+        (Get-DscSplattedResource -ResourceName SqlEndpoint -ExecutionName $executionName -Properties $value -NoInvoke).Invoke($value)
+    }
+}
