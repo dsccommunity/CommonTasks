56d0bc61c7d08c1775d9d5c0f76e4fb308ad6b1d
Daniel Gallinari <daniel.gallinari@gmx.com>
2022-03-09 20:16:13 +0100

    Added Robocopies and VSTSAgents. Fixed EnvironmentVariables

---
+++ source/DSCResources/Robocopies/Robocopies.psd1
@@ -0,0 +1,15 @@
+@{
+    RootModule           = 'Robocopies.schema.psm1'
+
+    ModuleVersion        = '0.0.1'
+
+    GUID                 = '0249017d-e4d4-4dcb-a0a1-6b28ef8967db'
+
+    Author               = 'NA'
+
+    CompanyName          = 'NA'
+
+    Copyright            = 'NA'
+
+    DscResourcesToExport = @('Robocopies')
+}
---
+++ source/DSCResources/Robocopies/Robocopies.schema.psm1
@@ -0,0 +1,18 @@
+Configuration Robocopies
+{
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [hashtable[]] $Items
+    )
+
+    Import-DscResource -ModuleName xRobocopy
+
+    foreach ($item in $Items)
+    {
+        $item = @{} + $item
+
+        $executionName = "xRobocopy_$($item.Destination)" -replace '[\s(){}/\\:-]', '_'
+        (Get-DscSplattedResource -ResourceName xRobocopy -ExecutionName $executionName -Properties $item -NoInvoke).Invoke($item)
+    }
+}
