58123fd812ef29d21965b2ab6cf8132dc5f02f15
Raimund Andree <r.andree@live.com>
2022-04-07 12:37:49 +0200

    Added support for CimInstance parameters

--- source/DSCResources/WebSites/Websites.schema.psm1
+++ source/DSCResources/WebSites/Websites.schema.psm1
@@ -8,6 +8,12 @@ configuration WebSites {
     Import-DscResource -ModuleName PSDesiredStateConfiguration
     Import-DscResource -ModuleName xWebAdministration
 
+    $cimClassParameters = @{
+        AuthenticationInfo = 'MSFT_xWebAuthenticationInformation'
+        BindingInfo        = 'MSFT_xWebBindingInformation'
+        LogCustomFields    = 'MSFT_xLogCustomFieldInformation'
+    }
+
     foreach ($item in $Items)
     {
         # Remove Case Sensitivity of ordered Dictionary or Hashtables
@@ -18,8 +24,16 @@ configuration WebSites {
             $item.Ensure = 'Present'
         }
 
-        $executionName = "website_$($item.Name -replace '[{}#\-\s]','_')"
+        foreach ($cimClassParameter in $cimClassParameters.GetEnumerator())
+        {
+            if ($item.ContainsKey($cimClassParameter.Name))
+            {
+                $data = $item."$($cimClassParameter.Name)"
+                $item."$($cimClassParameter.Name)" = (Get-DscSplattedResource -ResourceName $cimClassParameters."$($cimClassParameter.Name)" -Properties $data -NoInvoke).Invoke($data)
+            }
+        }
 
+        $executionName = "website_$($item.Name -replace '[{}#\-\s]','_')"
         (Get-DscSplattedResource -ResourceName xWebSite -ExecutionName $executionName -Properties $item -NoInvoke).Invoke($item)
     }
 }
