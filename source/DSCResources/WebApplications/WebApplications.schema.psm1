71b01a8d734c3df8f885033afae326deeb090439
Raimund Andree <r.andree@live.com>
2022-04-13 09:29:37 +0200

    Removing code that is no longer required due to latest changes in Get-DscSpattedResource

--- source/DSCResources/WebApplications/WebApplications.schema.psm1
+++ source/DSCResources/WebApplications/WebApplications.schema.psm1
@@ -20,19 +20,6 @@ configuration WebApplications {
             $item.Ensure = 'Present'
         }
 
-        foreach ($key in $item.Clone().Keys)
-        {
-            foreach ($subItem in $item.$key)
-            {
-                $scriptBlock = Get-DscCimInstanceReference -ResourceName $dscResourceName -Parameter $key -Data $subItem
-                if ($scriptBlock)
-                {
-                    $result = $scriptBlock.Invoke($subItem)
-                    $item[$key] = $result
-                }
-            }
-        }
-
         $executionName = "webapp_$($item.Name -replace '[{}#\-\s]','_')"
         (Get-DscSplattedResource -ResourceName $dscResourceName -ExecutionName $executionName -Properties $item -NoInvoke).Invoke($item)
     }
--- source/DSCResources/WebSites/Websites.schema.psm1
+++ source/DSCResources/WebSites/Websites.schema.psm1
@@ -20,19 +20,6 @@ configuration WebSites {
             $item.Ensure = 'Present'
         }
 
-        foreach ($key in $item.Clone().Keys)
-        {
-            foreach ($subItem in $item.$key)
-            {
-                $scriptBlock = Get-DscCimInstanceReference -ResourceName $dscResourceName -Parameter $key -Data $subItem
-                if ($scriptBlock)
-                {
-                    $result = $scriptBlock.Invoke($subItem)
-                    $item[$key] = $result
-                }
-            }
-        }
-
         $executionName = "website_$($item.Name -replace '[{}#\-\s]','_')"
         (Get-DscSplattedResource -ResourceName $dscResourceName -ExecutionName $executionName -Properties $item -NoInvoke).Invoke($item)
     }
