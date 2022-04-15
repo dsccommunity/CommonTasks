5fb12674ea3ff33c5a573c018277ef45208a19ac
Jan-Hendrik Peters [MSFT] <nyanhp@users.noreply.github.com>
2022-04-15 22:48:46 +0200

    New resource AddsTrust added (#165)
    * New resource AddsTrust added
    
    * AddsTrusts Docs added
    
    * Change encoding

---
+++ source/DSCResources/AddsTrusts/AddsTrusts.schema.psm1
@@ -0,0 +1,32 @@
+configuration AddsTrusts
+{
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [hashtable[]]
+        $Trusts
+    )
+
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+    Import-DscResource -ModuleName ActiveDirectoryDsc
+
+    $PSBoundParameters.Remove('InstanceName')
+    $PSBoundParameters.Remove('DependsOn')
+
+    foreach ($trust in $Trusts)
+    {
+        $waitFor = @{
+            DomainName = $trust.TargetDomainName
+        }
+        if ($trust.TargetCredential)
+        {
+            $waitFor.Credential = $trust.TargetCredential
+        }
+
+        (Get-DscSplattedResource -ResourceName WaitForADDomain -ExecutionName $trust.TargetDomainName -Properties $waitFor -NoInvoke).Invoke($waitFor)
+
+        $trust['DependsOn'] = "[WaitForADDomain]$($trust.TargetDomainName)"
+        $executionName = "$($trust.SourceDomainName)-to-$($trust.TargetDomainName)".Replace('.','-')
+        (Get-DscSplattedResource -ResourceName ADDomainTrust -ExecutionName $executionName -Properties $trust -NoInvoke).Invoke($trust)
+    }
+}
