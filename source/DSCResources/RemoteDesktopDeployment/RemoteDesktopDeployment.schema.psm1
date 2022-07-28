10dda173c68d6c6fe6bc6366c44c5b13cabce27b
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2022-07-28 15:58:11 +0200

    RemoteDesktopDeployment added

---
+++ source/DSCResources/RemoteDesktopDeployment/RemoteDesktopDeployment.schema.psm1
@@ -0,0 +1,38 @@
+﻿configuration RemoteDesktopDeployment
+{
+    param
+    (
+        [Parameter(Mandatory)]
+        [string]
+        $ConnectionBroker,
+
+        [Parameter()]
+        [string]
+        $WebAccess,
+
+        [Parameter()]
+        [string[]]
+        $SessionHosts,
+
+        [Parameter()]
+        [hashtable[]]
+        $Gateways
+    )
+
+    Import-DscResource -ModuleName xRemoteDesktopSessionHost
+
+
+    xRDSessionDeployment RDS
+    {
+        ConnectionBroker = $ConnectionBroker
+        WebAccessServer  = $WebAccess
+        SessionHost      = $SessionHosts
+    }
+
+    foreach ($gateway in $Gateways)
+    {
+        $executionName = "rdsgw_$($gateway.GatewayServer -replace '[().:\s]', '')"
+
+        (Get-DscSplattedResource -ResourceName xRDSessionDeployment -ExecutionName $executionName -Properties $gateway -NoInvoke).Invoke($gateway)
+    }
+}
