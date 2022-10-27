f59c36775229d6e9b8cb96153bc91359e5f9c503
Christian Küwen <chkuewen@microsoft.com>
2022-10-27 15:41:12 +0200

    Changelog and whitespace

--- source/DSCResources/SqlScriptQueries/SqlScriptQueries.schema.psm1
+++ source/DSCResources/SqlScriptQueries/SqlScriptQueries.schema.psm1
@@ -35,8 +35,6 @@ configuration SqlScriptQueries {
 
         $ByteString = [System.Text.Encoding]::UTF8.GetBytes(($query.ServerName+$query.InstanceName+$query.TestQuery+$query.SetQuery+$query.GetQuery))
         $hash = [System.BitConverter]::ToString($HashClass.ComputeHash($ByteString)) -replace '-',''
-
-
         $executionName = "SqlQuery_$($query.ServerName)_$($query.InstanceName)_$($hash)"
         (Get-DscSplattedResource -ResourceName SqlScriptQuery -ExecutionName $executionName -Properties $query -NoInvoke).Invoke($query)
     }
