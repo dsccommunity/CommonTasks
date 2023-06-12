4fa24d6a239a8aaf63899ab561c5cac2d3f57afb
Raimund Andree <r.andree@live.com>
2023-06-12 10:37:27 +0200

    CertificateRequests supports multiple certificates with the same issuer and subject by making friendlyName a mandatory (key) parameter

--- source/DSCResources/CertificateRequests/CertificateRequests.schema.psm1
+++ source/DSCResources/CertificateRequests/CertificateRequests.schema.psm1
@@ -8,27 +8,30 @@ configuration CertificateRequests
     )
 
     <#
-        Subject = [string]
-        [AutoRenew = [bool]]
-        [CARootName = [string]]
-        [CAServerFqdn = [string]]
-        [CAType = [string]]
-        [CepURL = [string]]
-        [CertificateTemplate = [string]]
-        [CesURL = [string]]
-        [Credential = [PSCredential]]
-        [DependsOn = [string[]]]
-        [Exportable = [bool]]
-        [FriendlyName = [string]]
-        [KeyLength = [string]{ 1024 | 192 | 2048 | 224 | 256 | 384 | 4096 | 521 | 8192 }]
-        [KeyType = [string]{ ECDH | RSA }]
-        [KeyUsage = [string]]
-        [OID = [string]]
-        [ProviderName = [string]]
-        [PsDscRunAsCredential = [PSCredential]]
-        [RequestType = [string]{ CMC | PKCS10 }]
-        [SubjectAltName = [string]]
-        [UseMachineContext = [bool]]
+        CertReq [String] #ResourceName
+        {
+            FriendlyName = [string]
+            Subject = [string]
+            [AutoRenew = [bool]]
+            [CARootName = [string]]
+            [CAServerFQDN = [string]]
+            [CAType = [string]]
+            [CepURL = [string]]
+            [CertificateTemplate = [string]]
+            [CesURL = [string]]
+            [Credential = [PSCredential]]
+            [DependsOn = [string[]]]
+            [Exportable = [bool]]
+            [KeyLength = [string]{ 1024 | 192 | 2048 | 224 | 256 | 384 | 4096 | 521 | 8192 }]
+            [KeyType = [string]{ ECDH | RSA }]
+            [KeyUsage = [string]]
+            [OID = [string]]
+            [ProviderName = [string]]
+            [PsDscRunAsCredential = [PSCredential]]
+            [RequestType = [string]{ CMC | PKCS10 }]
+            [SubjectAltName = [string]]
+            [UseMachineContext = [bool]]
+        }
     #>
 
     Import-DscResource -Module PSDesiredStateConfiguration
@@ -51,7 +54,7 @@ configuration CertificateRequests
         }
 
         $request.DependsOn = "[WaitForCertificateServices]WaitForADCS$($request.CARootName -replace '[\s(){}/\\:=-]', '_')"
-        $executionName = "req_$($request.Subject)" -replace '[\s(){}/\\:=\.-]', '_'
+        $executionName = "req_$($request.FriendlyName)_$($request.Subject)" -replace '[\s(){}/\\:=\.-]', '_'
         (Get-DscSplattedResource -ResourceName CertReq -ExecutionName $executionName -Properties $request -NoInvoke).Invoke($request)
     }
 }
